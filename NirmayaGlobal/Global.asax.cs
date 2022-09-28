95a7e98fca9f0058ab1fdab3d8e31d96e968b59b
Tadeochristopher Walker <114169915+tadeochristopher@users.noreply.github.com>
2022-09-28 12:56:01 -0400

    Add files via upload

---
+++ NirmayaGlobal/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NirmayaGlobal.App_Start;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+
+namespace NirmayaGlobal
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
