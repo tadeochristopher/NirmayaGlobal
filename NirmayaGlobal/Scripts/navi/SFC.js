5673de985d93a76d5b41658a4227bbd6bc4ec879
Tadeochristopher Walker <114169915+tadeochristopher@users.noreply.github.com>
2022-09-28 13:00:42 -0400

    Add files via upload

---
+++ NirmayaGlobal/Scripts/navi/SFC.js
@@ -0,0 +1,8 @@
+﻿var keptSafe0 = jQuery.noConflict();
+
+keptSafe0('.returnUp').click(function () {
+    keptSafe0('html, body').animate({
+        scrollTop: keptSafe0('.baseFH').offset().top
+    }, 'slow');
+    return false;
+});
